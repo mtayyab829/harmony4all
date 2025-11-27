9da839f9030aba6993eb48c4ce79d9f6f4b66b23
web-source-dev <muhammadnouman72321@gmail.com>
2025-11-27 12:57:24 +0800

    adjusted

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: false,
+    unoptimized: true,
     // Optimize image sizes for different device types
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
